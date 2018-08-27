45a2ff52a0972ed66792134bbfee0bcf9e49fc5f
Matthew Showers <MDSTardis42@gmail.com>
2018-08-27 14:56:19 -0500

    search

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,11 +38,16 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	while(temp != nullptr)
+	{
+		if(temp->getValue() == value)
+		{
+			return(true);
+		}
+		temp = temp->getNext();
+	}
 
-	return(isFound);
+	return(false);
 }
 
 template <typename T>
